bc52113a70706716b350c5f6559aa19345ed5ada
noppomario <9283305+noppomario@users.noreply.github.com>
2022-07-17 18:04:32 +0900

    helmet導入、API仕様書と競合するのでProductionのみ有効化

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const NODE_ENV = app.get(ConfigService).get('NODE_ENV');
 
   // ベースURL設定
   app.setGlobalPrefix('api');
@@ -16,12 +19,16 @@ async function bootstrap() {
   // 全URLでバリデーション有効化
   app.useGlobalPipes(new ValidationPipe());
 
+  // HTTPヘッダのセキュア化
+  if (NODE_ENV === 'production') {
+    app.use(helmet());
+  }
+
   // サーバ起動
   const PORT = app.get(ConfigService).get('PORT');
   await app.listen(PORT);
 
   // API仕様書のパスを表示(development)
-  const NODE_ENV = app.get(ConfigService).get('NODE_ENV');
   if (NODE_ENV === 'development') {
     // eslint-disable-next-line no-console
     console.log(`API documentation: http://localhost:${PORT}/openapi/`);
