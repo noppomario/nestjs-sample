921b653e9e76e3287af1322df20e7e56c3052c50
noppomario <9283305+noppomario@users.noreply.github.com>
2022-07-22 22:39:59 +0900

    環境変数のバリデーション処理を最低限追加

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { join } from 'path';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { GlobalConfigModule } from './modules/global-config/global-config.module';
 import { UsersModule } from './modules/users/users.module';
 import { LogsMiddleware } from './middlewares/logs.middleware';
@@ -12,7 +13,19 @@ import { LogsMiddleware } from './middlewares/logs.middleware';
 @Module({
   imports: [
     // 環境変数モジュール
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test')
+          .default('production'),
+        PORT: Joi.number(),
+      }),
+      validationOptions: {
+        allowUnknown: true,
+        abortEarly: true,
+      },
+    }),
 
     // 静的ホスティングモジュール
     ServeStaticModule.forRoot({
