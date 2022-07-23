d8f7b750d7fe6697947ef93c69d35c70a6bb8784
noppomario <9283305+noppomario@users.noreply.github.com>
2022-07-23 21:02:07 +0900

    コメント修正

--- src/utils/update-dto-fillter.ts
+++ src/utils/update-dto-fillter.ts
@@ -7,7 +7,7 @@ export class UpdateDtoFillter {
   /**
    * 引数で渡されたDTOから値がnullのプロパティを除去する
    *
-   * PartialTypeを継承した更新系DTOは、値がnullのプロパティを素通しする為、
+   * IsOptionalデコレータは値がnullのプロパティを素通しする為、
    * バリデーションロジックとは別に除去する必要がある。
    *
    * @param updateDto 更新データ
