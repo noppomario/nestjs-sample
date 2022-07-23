8235cb8b620a642ad5fcb93437750f77ae72810a
noppomario <9283305+noppomario@users.noreply.github.com>
2022-07-23 22:07:41 +0900

    prismaのログ出力を追加

--- src/modules/prisma/prisma-logsdb.service.ts
+++ src/modules/prisma/prisma-logsdb.service.ts
@@ -1,14 +1,43 @@
-import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import {
+  INestApplication,
+  Injectable,
+  Logger,
+  OnModuleInit,
+} from '@nestjs/common';
 // @internal内の事前に生成したclientを参照する
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { PrismaClient } from '@internal/prisma/client';
+import { Prisma, PrismaClient } from '@internal/prisma/client';
 
 /**
  * Prismaクライアント(logs.db)
  */
 @Injectable()
-export class PrismaLogsdbService extends PrismaClient implements OnModuleInit {
+export class PrismaLogsdbService
+  extends PrismaClient<Prisma.PrismaClientOptions, Prisma.LogLevel>
+  implements OnModuleInit
+{
+  private readonly logger = new Logger(PrismaLogsdbService.name);
+
+  constructor() {
+    super({ log: ['query', 'info', 'warn', 'error'] });
+  }
+
   async onModuleInit() {
+    this.$on('query', (event) => {
+      this.logger.log(`Query: ${event.query}`);
+      this.logger.log(`Params: ${event.params}`);
+      this.logger.log(`Duration: ${event.duration} ms`);
+    });
+    this.$on('info', (event) => {
+      this.logger.log(`message: ${event.message}`);
+    });
+    this.$on('error', (event) => {
+      this.logger.error(`error: ${event.message}`);
+    });
+    this.$on('warn', (event) => {
+      this.logger.warn(`warn: ${event.message}`);
+    });
+
     await this.$connect();
   }
 
--- src/modules/prisma/prisma-usersdb.service.ts
+++ src/modules/prisma/prisma-usersdb.service.ts
@@ -1,12 +1,41 @@
-import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import {
+  INestApplication,
+  Injectable,
+  Logger,
+  OnModuleInit,
+} from '@nestjs/common';
+import { Prisma, PrismaClient } from '@prisma/client';
 
 /**
  * Prismaクライアント(users.db)
  */
 @Injectable()
-export class PrismaUsersdbService extends PrismaClient implements OnModuleInit {
+export class PrismaUsersdbService
+  extends PrismaClient<Prisma.PrismaClientOptions, Prisma.LogLevel>
+  implements OnModuleInit
+{
+  private readonly logger = new Logger(PrismaUsersdbService.name);
+
+  constructor() {
+    super({ log: ['query', 'info', 'warn', 'error'] });
+  }
+
   async onModuleInit() {
+    this.$on('query', (event) => {
+      this.logger.log(`Query: ${event.query}`);
+      this.logger.log(`Params: ${event.params}`);
+      this.logger.log(`Duration: ${event.duration} ms`);
+    });
+    this.$on('info', (event) => {
+      this.logger.log(`message: ${event.message}`);
+    });
+    this.$on('error', (event) => {
+      this.logger.error(`error: ${event.message}`);
+    });
+    this.$on('warn', (event) => {
+      this.logger.warn(`warn: ${event.message}`);
+    });
+
     await this.$connect();
   }
 
